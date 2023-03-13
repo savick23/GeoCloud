283bcdda4bccade62c8b41cd0781c9262b935962
Савичев Александр <savick@mail.ru>
2023-03-13 13:39:27 +0300

    Инициализация проекта2

---
+++ Ciclop-v03/Ciclop-v03/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ciclop_v03.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
