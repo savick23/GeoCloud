915fe05a98a9ff6f168fdcaba2d111d5909d1484
Летуновский Сергей <lesev@mail.ru>
2023-03-16 17:48:15 +0300

    +COM settings

--- Modules/RmSolution.GeoCom/Acquisition/GeoComService.cs
+++ Modules/RmSolution.GeoCom/Acquisition/GeoComService.cs
@@ -6,13 +6,14 @@ namespace RmSolution.GeoCom
 {
     #region Using
     using System;
+    using System.IO.Ports;
     using System.Threading.Tasks;
     using RmSolution.Runtime;
     #endregion Using
 
     public class GeoComService : ModuleBase, IOServer
     {
-        public GeoComService(IRuntime runtime) : base(runtime)
+        public GeoComService(IRuntime runtime, GeoComAdapterSet adapter) : base(runtime)
         {
             Subscribe = new[] { MSG.RuntimeStarted }; 
         }
@@ -31,5 +32,27 @@ namespace RmSolution.GeoCom
             }
             return base.ExecuteProcess();
         }
+    }
+
+    public enum GeoComAdapterMode
+    {
+        Tcp, Com
+    }
+
+    public class GeoComAdapterSet
+    {
+        public GeoComAdapterMode? Mode { get; set; }
+        public string? Port { get; set; }
+
+        public string? Address { get; set; }
+        public string? Mask { get; set; }
+
+        public int? BaudRate { get; set; }
+        public int? DataBits { get; set; }
+        public float? StopBits { get; set; }
+        public Parity Parity { get; set; }
+        public string? FlowControl { get; set; }
+        public bool? FIFO { get; set; }
+        public string? Interface { get; set; }
     }
 }
