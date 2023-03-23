b302510bfca90af231e8aa436c4264a52b6ac669
Летуновский Сергей <lesev@mail.ru>
2023-03-23 11:55:46 +0300

    fix

--- Client/RmSolution.Cyclops/RmHttpClient.cs
+++ Client/RmSolution.Cyclops/RmHttpClient.cs
@@ -4,12 +4,22 @@
 //--------------------------------------------------------------------------------------------------
 namespace RmSolution.Cyclops
 {
+    using System;
+    using System.Net.Http.Json;
+    #region Using
     using System.Reflection;
+    using RmSolution.Data;
+    #endregion Using
 
     public class RmHttpClient : HttpClient
     {
         public static string Title => Assembly.GetExecutingAssembly()?.GetCustomAttributes<AssemblyProductAttribute>().FirstOrDefault()?.Product ?? "RmSolution.Cyclops";
         public static string Version => Assembly.GetExecutingAssembly().GetName()?.Version?.ToString(2) ?? "3.1";
         public static string DataServer => "http://localhost:8087/api/";
+
+        public async Task<dynamic?> Query(Type type)
+        {
+            return await this.GetFromJsonAsync(string.Concat(DataServer, "data/equipments"), type);
+        }
     }
 }
