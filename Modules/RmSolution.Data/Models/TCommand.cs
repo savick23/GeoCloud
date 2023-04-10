268ed07eec521c5237e79401c034ad1e03432da7
letunovskysv <62968494+letunovskysv@users.noreply.github.com>
2023-04-10 16:09:21 +0300

    +commands

---
+++ Modules/RmSolution.Data/Models/TCommand.cs
@@ -0,0 +1,19 @@
+﻿//--------------------------------------------------------------------------------------------------
+// (С) 2020-2023 ООО «РМ Солюшн». RM System Platform 3.1. Все права защищены.
+// Описание: TEquipment – Справочник оборудования.
+//--------------------------------------------------------------------------------------------------
+namespace RmSolution.Data
+{
+    #region Using
+    using RmSolution.DataAnnotations;
+    #endregion Using
+
+    [TObject("Команды", "commands", Ordinal = 103)]
+    public class TCommand : TCatalogTreeRow
+    {
+        [TColumn("Команда", Length = 256)]
+        public string? Command { get; set; }
+        [TColumn("Описание", Length = 1024, Nullable = true)]
+        public string? Description { get; set; }
+    }
+}
