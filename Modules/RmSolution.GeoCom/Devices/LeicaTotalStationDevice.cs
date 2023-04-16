f4e69009f3bb82d94dc4bd25116864d461c56fdb
letunovskysv <62968494+letunovskysv@users.noreply.github.com>
2023-04-17 01:26:51 +0300

    fix

--- Modules/RmSolution.GeoCom/Devices/LeicaTotalStationDevice.cs
+++ Modules/RmSolution.GeoCom/Devices/LeicaTotalStationDevice.cs
@@ -7,11 +7,14 @@ namespace RmSolution.Devices
 {
     #region Using
     using System;
+    using System.Globalization;
     using System.Linq;
+    using System.Runtime.Intrinsics.X86;
     using System.Text;
     using System.Text.RegularExpressions;
     using RmSolution.Data;
     using RmSolution.Devices.Leica;
+    using static System.Runtime.InteropServices.JavaScript.JSType;
     #endregion Using
 
     /// <summary> Тахеометр Leica.</summary>
@@ -110,6 +113,112 @@ namespace RmSolution.Devices
             return resp.Data;
         }
 
+        /// <summary> Getting the Leica specific instrument name.</summary>
+        /// <remarks> Gets the instrument name, for example: TS30 0,5".</remarks>
+        /// <example> mod3 dev 000001 CSV_GetInstrumentName </example>
+        public byte[]? CSV_GetInstrumentName()
+        {
+            var resp = Request(RequestString("%R1Q,5004:"));
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var name = resp.Text;
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Getting the instrument configuration.</summary>
+        /// <remarks> This function returns information about the class and the configuration type of the instrument.</remarks>
+        /// <example> mod3 dev 000001 CSV_GetDeviceConfig </example>
+        public byte[]? CSV_GetDeviceConfig()
+        {
+            var resp = Request(RequestString("%R1Q,5035:"));
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var devicePrecisionClass = resp.Value[0];
+                var deviceConfigurationType = resp.Value[1];
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Getting the RL type.</summary>
+        /// <remarks> This function returns information about the reflectorless and long range distance measurement (RL) of the sinstrument.</remarks>
+        /// <example> mod3 dev 000001 CSV_GetReflectorlessClass </example>
+        public byte[]? CSV_GetReflectorlessClass()
+        {
+            var resp = Request(RequestString("%R1Q,5100:")); // %R1P,0,0:0,2023,'04','11','00','23','1d'
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var reRefLessClass = resp.Value[0];
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Getting the date and time.</summary>
+        /// <remarks> Gets the current date and time of the instrument. The ASCII response is formatted corresponding to the data type DATIME.A possible response can look like this: %R1P,0,0:0,1996,'07', '19','10','13','2f' (see chapter ASCII data type declaration for further information).</remarks>
+        /// <example> mod3 dev 000001 CSV_GetDateTime </example>
+        public byte[]? CSV_GetDateTime()
+        {
+            var resp = Request(RequestString("%R1Q,5008:"));
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var dt = new DateTime((int)resp.Value[0], (int)resp.Value[1], (int)resp.Value[2], (int)resp.Value[3], (int)resp.Value[4], (int)resp.Value[5]);
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Setting the date and time.</summary>
+        /// <remarks> Sets the current date and time of the instrument.</remarks>
+        /// <example> mod3 dev 000001 CSV_SetDateTime 2023,'03','19','10','13','2f' </example>
+        public byte[]? CSV_SetDateTime(DateTime datetime)
+        {
+            var resp = Request(RequestString("%R1Q,5007:", datetime));
+            if (resp.ReturnCode == GRC.OK)
+            {
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Getting the software version.</summary>
+        /// <remarks> Returns the system software version.</remarks>
+        /// <example> mod3 dev 000001 CSV_GetSWVersion </example>
+        public byte[]? CSV_GetSWVersion()
+        {
+            var resp = Request(RequestString("%R1Q,5034:"));
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var ver = new Version((int)resp.Value[0], (int)resp.Value[1], (int)resp.Value[2]);
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Checking the available power.</summary>
+        /// <remarks> This command returns the capacity of the current power source and its source (internal or external).</remarks>
+        /// <example> mod3 dev 000001 CSV_CheckPower </example>
+        public byte[]? CSV_CheckPower()
+        {
+            var resp = Request(RequestString("%R1Q,5039:"));
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var unCapacity = resp.Value[0]; // Actual capacity [%]
+                var eActivePower = resp.Value[1]; // Actual power source
+                var ePowerSuggest = resp.Value[2]; // Not supported
+            }
+            return resp.Data;
+        }
+
+        /// <summary> Getting the temperature.</summary>
+        /// <remarks> Get the internal temperature of the instrument, measured on the Mainboard side. Values are reported in degrees Celsius.</remarks>
+        /// <example> mod3 dev 000001 CSV_GetIntTemp </example>
+        public byte[]? CSV_GetIntTemp()
+        {
+            var resp = Request(RequestString("%R1Q,5011:"));
+            if (resp.ReturnCode == GRC.OK)
+            {
+                var temp = resp.Value[0]; // %R1P,0,0:0,11.742000000000001
+            }
+            return resp.Data;
+        }
+
         #endregion CENTRAL SERVICES (CSV COMF)
 
         #region ELECTRONIC DISTANCE MEASUREMENT (EDM COMF)
@@ -134,7 +243,7 @@ namespace RmSolution.Devices
             var resp = Request(RequestString("%R1Q,1058:"));
             if (resp.ReturnCode == GRC.OK)
             {
-                var intensity = (EDM_EGLINTENSITY_TYPE)resp.Value;
+                var intensity = (EDM_EGLINTENSITY_TYPE)resp.Value[0];
             }
             return resp.Data;
         }
@@ -155,12 +264,17 @@ namespace RmSolution.Devices
 
         #region Private methods
 
+        static string ToByte(int n) => string.Concat('\'', n.ToString("x2"), '\'');
+
         /// <summary> Формирование ASCII строки данных для отправки на устройство.</summary>
         static byte[] RequestString(params object[] data) =>
             LF.Concat(Encoding.ASCII.GetBytes(string.Concat(data.Select(p =>
             {
                 if (p.GetType().IsEnum)
                     return (int)p;
+
+                if (p is DateTime dt)
+                    return string.Join(',', dt.Year, ToByte(dt.Month), ToByte(dt.Day), ToByte(dt.Hour), ToByte(dt.Minute), ToByte(dt.Second));
 
                 return p;
             }
@@ -195,7 +309,8 @@ namespace RmSolution.Devices
             public readonly byte[]? Data;
             public readonly string? Response;
             public readonly GRC ReturnCode = GRC.UNDEFINED;
-            public readonly long Value;
+            public readonly string? Text;
+            public readonly long[] Value;
 
             public XResponse(byte[]? data)
             {
@@ -208,9 +323,16 @@ namespace RmSolution.Devices
                     if (GRC_Resources.Errors.TryGetValue(ReturnCode, out var msg))
                         throw new Exception(string.Concat("[", (int)ReturnCode, "] ", msg));
 
-                    Value = long.TryParse(Regex.Match(Response, @"(?<=,)\d+$").Value, out var val) ? val : -1;
+                    Text = Regex.Match(Response, @"(?<=\d+,).*").Value;
+                    if (Text.StartsWith('"'/*string*/))
+                        Text = Text[1..^1];
+                    else
+                        Value = Text.Split(new char[] { ',' }).Select(n =>
+                            n.StartsWith('\''/*byte*/) ? long.Parse(n[1..^1], NumberStyles.HexNumber) : long.TryParse(n, out var val) ? val : -1L).ToArray();
                 }
             }
+
+            public override string ToString() => Response ?? "(null)";
         }
     }
 }
