20a3447de8220d9a05b9911743f1ce06889f8ddf
letunovskysv <62968494+letunovskysv@users.noreply.github.com>
2023-04-19 14:30:47 +0300

    fix

--- Modules/RmSolution.DataAccess/Controllers/console/TelnetHtmlStream.cs
+++ Modules/RmSolution.DataAccess/Controllers/console/TelnetHtmlStream.cs
@@ -31,18 +31,19 @@ namespace RmSolution.DataAccess
 
         public override int Read(byte[] buffer, int offset, int count)
         {
-            var data = _sock.Read();
-            int start;
-            for (start = 0; start < data.Length; start++)
-            {
-                if (data[start] == 255/*IAC*/)
-                {
-                    start += 2;
-                    continue;
-                }
-                break;
-            }
-            data = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(data, start, data.Length - start).Replace(" ", "&nbsp;").Replace("\r\n", "<br/>"));
+            //var data = _sock.Read();
+            //int start;
+            //for (start = 0; start < data.Length; start++)
+            //{
+            //    if (data[start] == 255/*IAC*/)
+            //    {
+            //        start += 2;
+            //        continue;
+            //    }
+            //    break;
+            //}
+          //  data = Encoding.UTF8.GetBytes(Encoding.UTF8.GetString(data, start, data.Length - start).Replace(" ", "&nbsp;").Replace("\r\n", "<br/>"));
+            var data = Encoding.UTF8.GetBytes("LETUNOVSKY SERGEY\r\n".Replace(" ", "&nbsp;").Replace("\r\n", "<br/>"));
             Array.Copy(data, buffer, data.Length);
             _length += data.Length;
             Position += data.Length;
