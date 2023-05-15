d6f5d7645c728fe5d253ef012649a018721d16cb
letunovskysv <62968494+letunovskysv@users.noreply.github.com>
2023-05-15 11:30:06 +0300

    +tags types

--- Modules/RmSolution.Data/Serialization/TType.cs
+++ Modules/RmSolution.Data/Serialization/TType.cs
@@ -1,97 +1,148 @@
 ﻿//--------------------------------------------------------------------------------------------------
 // (С) 2020-2023 ООО «РМ Солюшн». RM System Platform 3.1. Все права защищены.
-// Описание: TType – Типы.
+// Описание: TType – Типы объектов метаданных.
 //--------------------------------------------------------------------------------------------------
 namespace RmSolution.Data
 {
-    /// <summary> Типы.</summary>
-    public static class TType
+    #region Using
+    using System.Reflection;
+    #endregion Using
+
+    /// <summary> Типы объектов метаданных.</summary>
+    public struct TType : IComparable
     {
+        #region Constants
+
         /// <summary> Конфигурация.</summary>
-        public static readonly long Solution = 1;
+        public const long Solution = 1;
         /// <summary> Компоненты.</summary>
-        public static readonly long Component = 2;
+        public const long Component = 2;
         /// <summary> Объекты конфигурации (метаданных).</summary>
-        public static readonly long Object = 3;
+        public const long Object = 3;
         /// <summary> Простой тип.</summary>
-        public static readonly long SimpleType = 4;
+        public const long SimpleType = 4;
         /// <summary> Обработчик.</summary>
-        public static readonly long Handler = 5;
+        public const long Handler = 5;
         /// <summary> Приложение.</summary>
-        public static readonly long Application = 6;
+        public const long Application = 6;
         /// <summary> Модуль.</summary>
-        public static readonly long Module = 7;
+        public const long Module = 7;
         /// <summary> Настройки.</summary>
-        public static readonly long Settings = 8;
+        public const long Settings = 8;
         /// <summary> Константа.</summary>
-        public static readonly long Constant = 9;
+        public const long Constant = 9;
         /// <summary> Перечисление.</summary>
-        public static readonly long Enum = 10;
+        public const long Enum = 10;
         /// <summary> Справочник.</summary>
-        public static readonly long Catalog = 11;
+        public const long Catalog = 11;
         /// <summary> Документ.</summary>
-        public static readonly long Document = 12;
+        public const long Document = 12;
         /// <summary> Журнал.</summary>
-        public static readonly long Journal = 13;
+        public const long Journal = 13;
         /// <summary> Отчёт.</summary>
-        public static readonly long Report = 14;
+        public const long Report = 14;
         /// <summary> Регистр.</summary>
-        public static readonly long Register = 15;
+        public const long Register = 15;
         /// <summary> Регистр учётный.</summary>
-        public static readonly long Account = 16;
+        public const long Account = 16;
         /// <summary> Измерения для регистров.</summary>
-        public static readonly long Dimension = 17;
+        public const long Dimension = 17;
         /// <summary> Табличная часть, детали.</summary>
-        public static readonly long Details = 18;
+        public const long Details = 18;
         /// <summary> Запрос.</summary>
         /// <remarks> Заранее продготовленные запросы данных, представления.</remarks>
-        public static readonly long Query = 19;
+        public const long Query = 19;
         /// <summary> Реквизит.</summary>
-        public static readonly long Attribute = 20;
+        public const long Attribute = 20;
         /// <summary> Меню.</summary>
-        public static readonly long Menu = 21;
+        public const long Menu = 21;
         /// <summary> Панели инструментов.</summary>
-        public static readonly long Tool = 22;
+        public const long Tool = 22;
         /// <summary> Словарь тэгов.</summary>
-        public static readonly long Tag = 32;
+        public const long Tag = 32;
 
-        public static readonly long RefType = 65;
-        public static readonly long Byte = 65;
-        public static readonly long Int16 = 66;
-        public static readonly long Int32 = 67;
-        public static readonly long Int64 = 68;
-        public static readonly long Float = 69;
-        public static readonly long Double = 70;
-        public static readonly long Decimal = 71;
-        public static readonly long Numeric = 72;
-        public static readonly long Char = 73;
-        public static readonly long NChar = 74;
-        public static readonly long Varchar = 75;
-        public static readonly long NVarchar = 76;
-        public static readonly long DateTime = 77;
-        public static readonly long Date = 78;
-        public static readonly long Time = 79;
-        public static readonly long Boolean = 80;
-        public static readonly long Binary = 81;
-        public static readonly long VarBinary = 82;
-        public static readonly long Guid = 83;
-        public static readonly long TimeStamp = 84;
-        public static readonly long Array = 85;
-        public static readonly long Xml = 86;
-        public static readonly long Json = 87;
+        public const long RefType = 65;
+        public const long Byte = 65;
+        public const long Int16 = 66;
+        public const long Int32 = 67;
+        public const long Int64 = 68;
+        public const long Float = 69;
+        public const long Double = 70;
+        public const long Decimal = 71;
+        public const long Numeric = 72;
+        public const long Char = 73;
+        public const long NChar = 74;
+        public const long Varchar = 75;
+        public const long NVarchar = 76;
+        public const long DateTime = 77;
+        public const long Date = 78;
+        public const long Time = 79;
+        public const long Boolean = 80;
+        public const long Binary = 81;
+        public const long VarBinary = 82;
+        public const long Guid = 83;
+        public const long TimeStamp = 84;
+        public const long Array = 85;
+        public const long Xml = 86;
+        public const long Json = 87;
 
         /// <summary> Маска идентификатора объекта конфигурации.</summary>
         /// <remarks> 32767 возможных объекта конфигурации.</remarks>
-        public static readonly long TypeMask = 0x7FFF000000000000;
+        public const long TypeMask = 0x7FFF000000000000;
         /// <summary> Маска идентификатора строки.</summary>
         /// <remarks> 274 877 906 942 возможных записей в таблице для каждого объекта конфигурации.</remarks>
-        public static readonly long RecordMask = 0xFFFFFFFFFFFF;
+        public const long RecordMask = 0xFFFFFFFFFFFF;
         /// <summary> Маска идентификатора узла базы данных.</summary>
         /// <remarks> 1024 возможных узла баз данных в случае распределённой БД.</remarks>
-        public static readonly long NodeMask = 0x3FF;
+        public const long NodeMask = 0x3FF;
         /// <summary> Идентификатор новой записи.</summary>
-        public static readonly long NewId = 0xFFFFFFFFFC00;
+        public const long NewId = 0xFFFFFFFFFC00;
 
-        public static readonly long TypeIterator = 0x1000000000000;
+        public const long TypeIterator = 0x1000000000000;
+
+        #endregion Constants
+
+        long _value;
+
+        TType(long type)
+        {
+            _value = type;
+        }
+
+        public static TType Parse(string name)
+        {
+            return new TType((long)(typeof(TType).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .FirstOrDefault(f => f.Name.StartsWith(name, StringComparison.InvariantCultureIgnoreCase))?.GetValue(null) ?? 0));
+        }
+
+        public static implicit operator TType(long t) => new TType(t);
+
+        public static implicit operator long(TType t) => t._value;
+
+        public static bool operator ==(TType left, long right) => left._value == right;
+
+        public static bool operator !=(TType left, long right) => left._value != right;
+
+        public static bool operator ==(TType left, TType right) => left._value == right._value;
+
+        public static bool operator !=(TType left, TType right) => left._value != right._value;
+
+        public override bool Equals(object obj) => this == (TType)obj;
+
+        public override int GetHashCode() => HashCode.Combine(_value);
+
+        public int CompareTo(object obj)
+        {
+            long value = ((TType)obj)._value;
+            return _value < value ? -1 : _value == value ? 0 : 1;
+        }
+
+        public override string ToString()
+        {
+            long value = _value;
+            return typeof(TType).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+                .FirstOrDefault(f => f.GetValue(null).Equals(value))?.Name
+                ?? string.Concat("Unknow", _value);
+        }
     }
 }
---
+++ Modules/RmSolution.Data/Tags/TTag.cs
@@ -0,0 +1,148 @@
+﻿//--------------------------------------------------------------------------------------------------
+// (С) 2020-2023 ООО «РМ Солюшн». RM System Platform 3.1. Все права защищены.
+// Описание: TagBase – Базовый тэг данных.
+//--------------------------------------------------------------------------------------------------
+namespace RmSolution.Data
+{
+    #region Using
+    using System;
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+    using System.Text.Json;
+    using System.Text.Json.Serialization;
+    using RmSolution.DataAnnotations;
+    #endregion Using
+
+    /// <summary> Базовый тип тэга. </summary>
+    public abstract class TTag : TEntity
+    {
+        /// <summary> Короткий 32-разрядный ИД.</summary>
+        public int TagId => (int)Id;
+        /// <summary> Площадка. Вычисляемое поле</summary>
+        public long Site { get; set; }
+        /// <summary> Код типа сообщения из справочника.</summary>
+        public long EventType { get; set; }
+        public long DataType { get; set; }
+        /// <summary> Источник данных. Оборудование. OPC Сервер.</summary>
+        public long Provider { get; set; }
+        /// <summary> Способ сохранения истории значений тэгов.</summary>
+        public StorageMethod Storage { get; set; }
+        /// <summary> Канал ввода/вывода, сигнал.</summary>
+        public int Channel { get; set; }
+
+        /// <summary> Отключение сигнализации. По умолчанию включена.</summary>
+        public bool AlarmEnable { get; set; } = true;
+        /// <summary> Список тревог.</summary>
+        public TAlarmTag[] Alarms { get; set; }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long Hash(long kind, int channel = 0) => (kind << 8) + channel;
+
+        /// <summary> Тип данных C#.</summary>
+        public Type CType =>
+            DataType switch
+            {
+                TType.Int16 => typeof(short),
+                TType.Int32 => typeof(int),
+                TType.Int64 => typeof(long),
+                TType.Boolean => typeof(bool),
+                TType.Float => typeof(float),
+                TType.Double => typeof(double),
+                TType.DateTime => typeof(DateTime),
+                TType.Varchar => typeof(string),
+                _ => typeof(long)
+            };
+
+        /// <summary> Преобразует в строку описания для сохранения в БД (поле Definition).</summary>
+        public string ToDefinition() =>
+            $"{{\"Id\":{Id},\"Parent\":{Parent},\"Type\":{(long)Type},\"Provider\":{Provider},\"Channel\":\"{Channel}\",\"Code\":\"{Code}\",\"Name\":\"{Name}\""
+            + (string.IsNullOrWhiteSpace(Description) ? string.Empty : $",\"Description\":\"{Description}\"")
+            + "}";
+    }
+
+    public sealed class TAnalogTag : TTag
+    {
+        public double? MinValue { get; set; }
+        public double? MaxValue { get; set; }
+        public double? Deadband { get; set; }
+    }
+
+    public sealed class TDiscreteTag : TTag
+    {
+    }
+
+    public sealed class TStringTag : TTag
+    {
+    }
+
+    public sealed class TEventTag : TTag
+    {
+    }
+
+    public sealed class TAlarmTag : ICloneable
+    {
+        public int TagId { get; set; }
+        public DateTime DateTime { get; set; }
+        public AlarmTagType Type { get; set; }
+        public long Class { get; set; }
+        public int Priority { get; set; }
+        public double Value { get; set; }
+        public string Message { get; set; }
+
+        public long? Location { get; set; }
+        public string Picket { get; set; }
+        public long? Pass { get; set; }
+        public long? Provider { get; set; }
+
+        readonly static MethodInfo _memberwiseClone = typeof(TAlarmTag).GetMethod("MemberwiseClone", BindingFlags.Instance | BindingFlags.NonPublic);
+        public object Clone() => _memberwiseClone.Invoke(this, null);
+    }
+
+    public enum TagEventType : long
+    {
+        /// <summary> Превышено минимальное значение.</summary>
+        Minimum = 1,
+        /// <summary> Возврат к нормальному значению.</summary>
+        Normal = 2,
+        /// <summary> Превышено максимальное значение.</summary>
+        Maximum = 3
+    }
+
+    /// <summary> Способы сохранения истории значений тэгов.</summary>
+    public enum StorageMethod
+    {
+        /// <summary> Не сохранять значения.</summary>
+        NotStored = 0,
+        /// <summary> Циклическая запись с заданным интервалом.</summary>
+        Cyclic = 1,
+        /// <summary> Сохранение изменений от предыдущего значения.</summary>
+        Delta = 2,
+        /// <summary> Сохранение всех изменений значений.</summary>
+        Forced = 3
+    }
+
+    [JsonConverter(typeof(JsonAlarmTagTypeConverter))]
+    public enum AlarmTagType
+    {
+        Normal = 0,
+        LoLo = 10, Low = 11, HiHi = 12, High = 13, // для аналоговых тэгов
+        On = 20, Off = 21,                         // для дискретных тэгов
+        Action = 30, Close = 31, Ack = 32          // для событийных тэгов
+    }
+
+    sealed class JsonAlarmTagTypeConverter : JsonConverter<AlarmTagType>
+    {
+        public override AlarmTagType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+                return (AlarmTagType)Enum.Parse(typeof(AlarmTagType), reader.GetString(), true);
+
+            return (AlarmTagType)reader.GetInt32();
+        }
+
+        public override void Write(Utf8JsonWriter writer, AlarmTagType value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
